f4adfe4719fd6e7000bf4af7abbd4d7603793a5b
“helti” <helti@qq.com>
2023-06-03 16:21:12 +0800

    feat: 添加百度统计

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var _hmt = _hmt || [];(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?efa73e07deb6bf9fe2148b7c6c6134f8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();`,
+          }}
+        ></script>
+      </Head>
       <body className=" bg-gray-10 dark:bg-slate-900">
         <Main />
         <NextScript />
